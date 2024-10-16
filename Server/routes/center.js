b97e3b5b6fd7fca27090c978e556de6d659c8451
Naman-Pundir <namanpundir122@gmail.com>
2024-10-17 05:15:50 +0530

    New

--- Server/routes/center.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const centerController = require('../controllers/centerController');
-
-router.get('/:center_id/sports',centerController.getSportsByCenter);
-router.get('/', centerController.getAllCenters);
-
-module.exports = router;
--- Server/routes/court.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const courtController = require('../controllers/courtController')
-
-router.get('/:sport_id',courtController.getCourtsBySport);
-
-module.exports = router;
